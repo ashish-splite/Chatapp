54a5ce9ee6cd03580345002eae19699edad9dd4e
ashish-splite <160389187+ashish-splite@users.noreply.github.com>
2024-03-24 00:39:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hey, Talk to me</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <script>
+        {(function (d, t) {
+          var BASE_URL = "http://chatwoot-rails-api-service:80";
+          var CDN_URL = "https://chat-widget.hiverhq.com/chat-widget";
+          var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+          g.src = CDN_URL + "/js/sdk.js";
+          g.defer = true;
+          g.async = true;
+          s.parentNode.insertBefore(g, s);
+          g.onload = function () {
+            window.chatwootSDK.run({
+              websiteToken: "zLAch6srF3nowxuFzWtAsMPR",
+              baseUrl: BASE_URL,
+            });
+          };
+        })(document, "script")}
+      </script>
+    </div>
+  );
+}
+
+export default App;
